d88f55973be7d299c59001c50e165b0fc2dee84c
steveleblanc <steveleblanc77@gmail.com>
2017-03-05 22:48:13 -0500

    Cleaning up

--- index.php
+++ index.php
@@ -1,44 +1,36 @@
-<?php 
-include "navbar.php";
- ?>
+<?php include "navbar.php"; ?>
 
 <div class="container">
-  <div class="row">
-<form class="form-horizontal" action="file-upload-2.php" method="post" enctype="multipart/form-data">
-<fieldset>
+    <div class="row">
+        <form class="form-horizontal" action="file-upload.php" method="post" enctype="multipart/form-data">
+            <fieldset>
 
-<!-- Form Name -->
-<legend>Choose a file from your computer to upload to us</legend>
+                <!-- Form Name -->
+                <legend>Upload a file from your computer to us</legend>
 
-<!-- File Button --> 
-<div class="form-group">
-  <label class="col-md-12 control-label" for="filebutton"></label>
-  <div class="col-md-12">
-    <input id="fileToUpload" name="fileToUpload" class="input-file" type="file">
-  </div>
+                <!-- File Button -->
+                <div class="form-group">
+                    <label class="col-md-1 control-label" for="filebutton">Step #1</label>
+                    <div class="col-md-4">
+                        <input id="fileToUpload" name="fileToUpload" class="input-file" type="file">
+                    </div>
+                </div>
+
+                <!-- Button -->
+                <div class="form-group">
+                    <label class="col-md-1 control-label" for="singlebutton">Step #2</label>
+                    <div class="col-md-4">
+                        <input type="submit" class="btn btn-primary" value="Upload Image" name="submit">
+                    </div>
+                </div>
+            </fieldset>
+        </form>
+        <div class="col-md-12">
+            <legend>Here is your list of uploaded files so far</legend>
+            <div class="col-md-12">
+                <?php //Scan the uploads folder and return a list of the uploaded files
+                $files=array_slice(scandir( 'uploads/'), 2); echo implode( "<p> </p>",$files); ?>
+            </div>
+        </div>
+    </div>
 </div>
-
-<!-- Button -->
-<div class="form-group">
-  <label class="col-md-12 control-label" for="singlebutton"></label>
-  <div class="col-md-12">
-    <input type="submit" class="btn btn-primary" value="Upload Image" name="submit">
-  </div>
-</div>
-</fieldset>
-</form>
-<div  class="col-md-12">
-  <legend>Here is your list of uploaded files so far</legend>
-  <div class="col-md-12">
-    <?php
-    //Scan the uploads folder and return a list of the uploaded files
-$files = array_slice(scandir('uploads/'), 2);
-echo implode(" <br><br>",$files);
-?>
-  </div>
-</div>
-</div>
-</div>
-
-
-
--- navbar.php
+++ navbar.php
@@ -1,40 +1,56 @@
 <html>
+
 <head>
-  <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+    crossorigin="anonymous">
 
 
- <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<script src="sorttable.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+    integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+    crossorigin="anonymous"></script>
+    <script src="sorttable.js"></script>
 </head>
 <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="/support">Customer Support</a>
-    </div>    
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="/help">Customer Support Home</a></li>
-            <li><a href="/help/fileupload/">Upload A File</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed"
+            data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+            aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/support">Customer Support</a>
+        </div>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Link</a>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"
+                role="button" aria-haspopup="true"
+                aria-expanded="true">Dropdown <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="/help">Customer Support Home</a>
+                    </li>
+                    <li><a href="/help/fileupload/">Upload A File</a>
+                    </li>
+                    <li><a href="#">Something else here</a>
+                    </li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">Separated link</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+    <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
 </nav>
